06775a260a8ade7d80454aec1474ebdb413e8825
Daniel André Eikeland <zegenie@gmail.com>
2016-01-31 20:40:07 +0100

    initial commit

---
+++ Api.php
@@ -0,0 +1,67 @@
+<?php
+
+    namespace thebuggenie\modules\api;
+
+    use thebuggenie\core\framework;
+
+    /**
+     * The Bug Genie API module
+     *
+     * @author
+     * @version 0.1
+     * @license http://opensource.org/licenses/MPL-2.0 Mozilla Public License 2.0 (MPL 2.0)
+     * @package thebuggenie/modules/api
+     * @subpackage core
+     */
+
+    /**
+     * The Bug Genie API module
+     *
+     * @package thebuggenie/modules/api
+     * @subpackage core
+     *
+     * @Table(name="\thebuggenie\core\entities\tables\Modules")
+     */
+    class Api extends \thebuggenie\core\entities\Module
+    {
+
+        const VERSION = '0.1';
+
+        protected $_name = 'api';
+        protected $_longname = 'External API module';
+        protected $_description = 'Enables integration with third party tools and plugins';
+        protected $_module_config_title = 'API';
+        protected $_module_config_description = 'Set up the API module from this section';
+        protected $_has_account_settings = false;
+
+        /**
+         * Return an instance of this module
+         *
+         * @return Api
+         */
+        public static function getModule()
+        {
+            return framework\Context::getModule('api');
+        }
+
+        protected function _initialize()
+        {
+        }
+
+        protected function _addListeners()
+        {
+        }
+
+        protected function _install($scope)
+        {
+        }
+
+        protected function _loadFixtures($scope)
+        {
+        }
+
+        protected function _uninstall()
+        {
+        }
+
+    }
---
+++ README.md
@@ -0,0 +1,15 @@
+# The Bug Genie API integration module
+
+## Installation
+
+### 1: The Bug Genie module installation
+
+Clone this repository either straight into a folder under `thebuggenie/modules/api`,
+or symlink it to the same folder. Then enable the module from the configuration section in
+The Bug Genie.
+
+
+## Reporting issues
+
+If you find any issues, please report them in the issue tracker on our website:
+http://issues.thebuggenie.com/module-api
